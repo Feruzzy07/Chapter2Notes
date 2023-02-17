674bd49d0b1fce2b14f630e0547fe731797b1db7
526o <526o@fresnou.org>
2023-02-17 14:14:22 -0800

    initial commit

---
+++ whileLoops1.js
@@ -0,0 +1,40 @@
+const readLine = require('readline-sync');
+
+//Question 1
+// let a = 0;
+// while (a <= 25){
+//     console.log(a);
+//     a++;
+// }
+
+//Question 2
+// let b = 0;
+// while (b <= 50){
+//     console.log(b);
+//     b++;
+// }
+
+// //Question 3
+// let c = -10;
+// while (c < 11){
+//     console.log(c);
+//     c++;
+// }
+
+// //Question 4
+// let d = 0;
+// while (d > -51){
+//     console.log(d);
+//     d--;
+// }
+
+//Question 5
+// let e = 0;
+// while (e <= 100){
+//     if (e % 2 == 0){
+//         console.log(e);
+//     }
+//     e++;
+// }
+
+//Question 6
