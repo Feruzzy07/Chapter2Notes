1655e3cc1acff314350c33aa3fa8aec615ebfccd
526o <526o@fresnou.org>
2023-02-23 14:18:48 -0800

    finished forLoops 1

---
+++ forLoops1.js
@@ -0,0 +1,93 @@
+const readLine = require('readline-sync');
+
+// //Question 1
+// for(let i=0; i <=150; i++){
+//     console.log(i);
+// }
+
+//Question 2
+// for(let i=0; i <=40; i++){
+//     console.log(i*i);
+// }
+
+//Question 3
+// for(let i=0; i <=30; i++){
+//     console.log(i*i*i);
+// }
+
+//Question 4
+// for(let i=0; i<=700; i++){
+//     if(i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+//Question 5
+// for(let i=350; i<=1000; i++){
+//     if(i % 10 == 0){
+//         console.log(i);
+//     }
+// }
+
+//Question 6
+// let sum = 1;
+// for (let i=1; i <=50; i++){
+//         sum += i;
+// }
+// console.log(sum);
+
+//Question 7
+// let sum = 35;
+// for (let i=35; i <=70; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//Question 8
+// let product = 1;
+// for (let i=1; i <=15; i++){
+//     product *= i;
+// }
+// console.log(product);
+
+//Question 9
+// let product = 10;
+// for (let i=10; i <=35; i++){
+//     product *= i;
+// }
+// console.log(product);
+
+//Question 10
+// let sum = 1;
+// for (let i=1; i <=12; i++) {
+//     if (i <= 12) {
+//         console.log("Happy Holidays!");
+//     }
+// }
+
+//Question 11
+// let number = readLine.question("Give me a number: ");
+// let i = 1;
+// for (let i=1; i <=number; i++){
+//     if (i <=number){
+//         console.log("Happy Holidays!");
+//     }
+// }
+
+//Question 12
+// let sum = 100;
+// for (let i=100; i <=200; i++){
+//     if(i % 2 ==1){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+//Question 13
+// let product = 25;
+// for (let i=25; i <=50; i++){
+//     if(i % 2 ==1){
+//         product *= i;
+//     }
+// }
+// console.log(product);
