7010192253beb107a07cb2aaf554631f42ad8735
526o <526o@fresnou.org>
2023-03-07 14:12:19 -0800

    finished buzz + question 1

---
+++ fizzbuzz.js
@@ -0,0 +1,37 @@
+const readLine = require('readline-sync');
+
+// let i = 1;
+// while(i <= 100){
+//     if(i % 15 == 0){
+//         console.log("Fizzbuzz");
+//     }else if(i % 3 == 0){
+//         console.log("Fizz");
+//     }else if (i % 5 == 0){
+//         console.log("Buzz");
+//     }else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//Question 1
+// let i = 1;
+// let number = Number(readLine.question("Give me a number: "));
+// while(i <= number){
+//     if(i % 15 == 0){
+//         console.log("Fizzbuzz");
+//     }else if(i % 3 == 0){
+//         console.log("Fizz");
+//     }else if (i % 5 == 0){
+//         console.log("Buzz");
+//     }else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+// while (number < 0){
+//     console.log("Invalid Answer.");
+//     number = Number(readLine.question("Try Again. Please type in a positive number: "));
+// }
+
+//Question 2
